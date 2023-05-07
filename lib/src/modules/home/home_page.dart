dc55844ac04e8c1b4cf42ffde48031b47d52a9ef
ElcioLS <elciols@gmail.com>
2023-05-07 00:26:45 -0300

    Hepers Messages

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../core/env/env.dart';
 import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,7 +11,7 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with Loader {
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +23,8 @@ class _HomePageState extends State<HomePage> with Loader {
           showLoader();
           await Future.delayed(const Duration(seconds: 5));
           hideLoader();
+          // showError('Erro qualquer');
+          showWarning('atenção');
         },
       ),
       body: Column(
